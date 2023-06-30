b03afd5ef40f33c6f34da874fce3284a75594149
strovertz <gleisonaps@hotmail.com>
2023-06-29 21:31:06 -0300

    atualizando pythons
    ;

--- automations/insert_data.py
+++ automations/insert_data.py
@@ -13,11 +13,12 @@ def insert_data(nome_tabela, caminho_csv):
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor()
 
-    cursor.execute(f"CREATE TABLE {nome_tabela} (coluna1 TIPO, coluna2 TIPO, ...)")
-
     with open(caminho_csv, 'r') as arquivo_csv:
         leitor_csv = csv.reader(arquivo_csv)
-        next(leitor_csv) 
+        cabecalho = next(leitor_csv)  # pega o cabecalho do csv
+        # cria a tabela
+        colunas = ','.join([f'{nome_coluna} TIPO' for nome_coluna in cabecalho])
+        cursor.execute(f"CREATE TABLE {nome_tabela} ({colunas})")
 
         for linha in leitor_csv:
             valores = ','.join('%s' for _ in linha)
@@ -26,8 +27,11 @@ def insert_data(nome_tabela, caminho_csv):
 
     conn.commit()
     conn.close()
+
 def main():
     insert_data('titles', '/tmp/titles.csv')
     insert_data('imdb_rating', '/tmp/credits.csv')
     insert_data('cast', '/tmp/person.csv')
     print('Dados importados com sucesso!')
+
+main()
--- tsrg/variables.tf
+++ tsrg/variables.tf
@@ -35,98 +35,63 @@ variable "vpc_default" {
 }
 
 variable "aws_secgrp_port" {
-
   type = number
-
   description = "Portas to|from"
-
   default = 80
-
 }
 
 variable "cidr_blocks" {
-
   type = string
-
   description = "when cidr_blocks equals '0.0.0.0/0'"
-
   default = "0.0.0.0/0"
 }
 
 variable "cidr_blocks_ssh" {
-
   type = string
-
   description = "when cidr_blocks is defined by usr'"
-
   default = "0.0.0.0/0"
 }
 
 variable "tcp" {
-
   type = string
-
   description = "tcp"
-
   default = "tcp"
 }
 
 variable "instance_ami" {
-
   type = string
-
   description = "AMI do ubuntu"
-
   default = "ami-052efd3df9dad4825"
 }
 
 variable "instance_type" {
-
   type = string
-
   description = "Instance type"
-
   default = "t3.micro"
 }
 
 variable "instance_tags" {
 
   type = map(string)
-
   description = "tags"
-
   default = {
-
-    Name = "Servidor-grafana"
-
-    Project = "teste"
-
+    Name = "Servidor-grafana-mysql"
+    Project = "Fundamentos-banco-dados"
   }
 }
 
 variable "PRIVATE_KEY_PATH" {
-  default = "gleison"
+  default = "~/.ssh/id_rsa"
 }
 
 variable "PUBLIC_KEY_PATH" {
-  default = "gleison.pub"
+  default = "~/.ssh/id_rsa.pub"
 }
 
 variable "EC2_USER" {
   default = "ubuntu"
 }
 
-/*variable "ec2" {
-  description = "The attribute of EC2 information"
-  type = object({
-    name              = "ppshein"
-    os_type           = "ubuntu"
-    instance_type     = "t2.micro"
-    volume_size       = 20
-    volume_type       = "gp3"
-    availability_zone = "us-east-1a"
-  })
-}*/
 variable "os_type" {
   type    = string
   default = "ubuntu"
